9c76e3cef27ca116174d60d91fad54a004803190
Simone van Corbach <simonevancorbach@gmail.com>
2026-07-01 15:01:03 +0200

    practicing with variables

---
+++ variables/target/debug/deps/variables-14ec9d06bbda1e8b.d
@@ -0,0 +1,5 @@
+/Users/simone/rust_projects/variables/target/debug/deps/variables-14ec9d06bbda1e8b.d: src/main.rs
+
+/Users/simone/rust_projects/variables/target/debug/deps/variables-14ec9d06bbda1e8b: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/simone/rust_projects/variables/target/debug/variables: /Users/simone/rust_projects/variables/src/main.rs
